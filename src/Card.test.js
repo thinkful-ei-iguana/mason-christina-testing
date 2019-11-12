9fdac6e143fbeb91c376739a245cc5a6e30aa506
Mason Reichbauer <mreichba@mail.usf.edu>
2019-11-12 12:43:13 -0500

    on snap test

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Card'
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Card />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+
+it('renders the UI as expected', () => {
+const tree = renderer
+    .create(<Card card= {[{title: 'titleHere', content: 'contentHere'},{title: 'look at me', content: 'this new content'}]}/>)
+    .toJSON();
+expect(tree).toMatchSnapshot();  
+});
--- src/List.test.js
+++ src/List.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List'
-
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<List cards= {[{title: 'titleHere', content: 'contentHere'}, {title: 'look at me', content: 'this new content'}]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+
+// it('renders the UI as expected', () => {
+// const tree = renderer
+//     .create(<List cards= {[{title: 'titleHere', content: 'contentHere'}, {title: 'look at me', content: 'this new content'}]}/>)
+//     .toJSON();
+// expect(tree).toMatchSnapshot();  
+// });
