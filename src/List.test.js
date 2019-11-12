f0538b8883cfa5562bd8bbbc51f6371e4a230045
Mason Reichbauer <mreichba@mail.usf.edu>
2019-11-12 11:54:02 -0500

    passed smoke test

--- src/List.test.js
+++ src/List.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List'
 
+
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<List />, div);
+    ReactDOM.render(<List cards= {[{title: 'titleHere', content: 'contentHere'}, {title: 'look at me', content: 'this new content'}]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
